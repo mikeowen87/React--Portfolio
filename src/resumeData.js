7b5e3dc4a7c7fa5e79714018094a6f85d203e583
mikeowen87 <mikeowen_87@yahoo.com>
2021-01-31 21:41:22 -0500

    First Commit

---
+++ src/resumeData.js
@@ -0,0 +1,86 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "Michael Owen",
+    "role": "Full Stack Web Developer",
+    "linkedinId":"Your LinkedIn Id",
+        "roleDescription": "Recently completed a Full-Stack Coding Bootcamp through Case Western Reserve University. Looking to grow into the web developing world.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/michael-owen-a5077291/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/mikeowen87",
+          "className":"fa fa-github"
+        },
+       
+      ],
+    "aboutme":"I am currently finishing up my Full Stack Web Developing program through Case Western Reserve University and am looking forward to becoming a future web developer and change my career field.",
+    "address":"Highland Heights, OH 44143",
+    "website":"",
+    "education":[
+      {
+        "UniversityName":"Case Western Reserve University",
+        "specialization":"Full Stack Web Development  ",
+        "MonthOfPassing":"Feb",
+        "YearOfPassing":"2021",
+        "Achievements":"Succesfuly completed the coding program and met all requirments."
+      },
+      {
+        "UniversityName":"University of Phoenix",
+        "specialization":"Computer Science",
+        "MonthOfPassing":"Current",
+        "YearOfPassing":"Current",
+        "Achievements":"Completing the Bachelors Program"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"OMNI Systems",
+        "specialization":"Flex Press Operator",
+        "MonthOfLeaving":"Current",
+        "YearOfLeaving":"Current",
+        "Achievements":"Top Operator, Completed all training and computer courses"
+      },
+      {
+        "CompanyName":"The Lincoln Electric Company",
+        "specialization":"Machine Operator, Welder, Lead",
+        "MonthOfLeaving":"Nov",
+        "YearOfLeaving":"2019",
+        "Achievements":"One of the highest production performers within company"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+       "skillname":"Reactjs"
+      }    ],
+  "portfolio":[
+  {
+        "name":"Where2Watch",
+        "description":"Simple site/app that allows users to find where movies or tv shows are to watch, i.e. Netflix, Prime, HULU or Disney+",
+        "imgurl":""      },
+    {
+        "name":"Quarantini",
+        "description":"App that allows you to find mixed cocktails and how to make them at home.",
+        "imgurl":""
+    },
+     {
+        "name":"TurnUp",
+        "description":"App that allows users to find and sign up for pop up shop events.",  
+        "imgurl":""
+    }
+      ],
+
+    }
+
+
+    
